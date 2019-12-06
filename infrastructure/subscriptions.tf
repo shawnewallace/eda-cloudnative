1e3fc5e19b6e4535a47fa4d3a606d4b0ecd7ab9a
Shawn Wallace <shawn@the-wallaces.net>
2019-12-06 14:23:53 -0500

    demonstrated how to forward messages

--- infrastructure/subscriptions.tf
+++ infrastructure/subscriptions.tf
@@ -1,8 +1,17 @@
 resource "azurerm_servicebus_subscription" "firstEvent-logger" {
-  name                = "logger"
+  name                = "logger1"
   resource_group_name = azurerm_resource_group.eda.name
   namespace_name      = azurerm_servicebus_namespace.eda.name
-  topic_name          = azurerm_servicebus_topic.firstEvent.name,
+  topic_name          = azurerm_servicebus_topic.firstEvent.name
+  forward_to = azurerm_servicebus_topic.loggerEvent.name
+  max_delivery_count  = 1
+} 
+
+resource "azurerm_servicebus_subscription" "secondEvent-logger" {
+  name                = "logger2"
+  resource_group_name = azurerm_resource_group.eda.name
+  namespace_name      = azurerm_servicebus_namespace.eda.name
+  topic_name          = azurerm_servicebus_topic.secondEvent.name
   forward_to = azurerm_servicebus_topic.loggerEvent.name
   max_delivery_count  = 1
 } 
